81238a28e8ac18a8424aead57ce2119ca17e8705
Stevie Snape <smasnape@gmail.com>
2018-02-06 23:13:04 -0500

    Some node foolery

--- demo.js
+++ demo.js
@@ -1,5 +1,17 @@
-//Random Number generator using lodash
+// //Random Number generator using lodash
+//
+// var _ = require('lodash')
+//
+// console.log(_.random(1,10054))
 
-var _ = require('lodash')
 
-console.log(_.random(1,10054))
+var fs = require('fs');
+
+
+// access the file system with read file, used utf-8 to specify data
+
+fs.readFile('./data.json', 'utf-8',(err,data) => {
+
+    console.log(data);
+
+})
--- sync-demo.js
+++ sync-demo.js
@@ -1,3 +1,4 @@
+
 fs = require('fs');
 
 
